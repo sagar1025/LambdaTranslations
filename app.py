05fe16e173b7a69dbbd5ba0706a7597e613e64c1
sagar1025 <sagarpatel1025@gmail.com>
2021-01-09 22:12:57 -0500

    init commit

---
+++ app.py
@@ -0,0 +1,160 @@
+from flask import Flask, request, jsonify, abort
+from flask_cors import CORS, cross_origin
+import json
+import logging
+import boto3
+from boto3.dynamodb.conditions import Key
+from botocore.exceptions import ClientError
+
+app = Flask(__name__)
+
+validLangs = ["ar","fr"]
+
+#decorator to validate each api call
+def Validate_API_Key(func):
+    @wraps(func)
+    def decorated_function(*args, **kwargs):
+        #print(os.getenv('ApiKey'))
+        if request.headers.get('Authorization'):
+            token_header = request.headers['Authorization']
+            auth_token = token_header.split(maxsplit=1)[1] #this removes the Bearer part of the value
+            if auth_token == os.getenv('ApiKey'):
+                return func(*args, **kwargs)
+            else:
+                logging.error('invalid APIKEY in header.')
+                logging.error(request.headers.get('Authorization'))
+                abort(401)
+        else:
+            logging.error('Auth header not present')
+            logging.error(request.headers.get('Authorization'))
+            abort(401)
+    return decorated_function
+
+def GetTranslation(txt, lang):
+    #call to AWS translate
+    boto_session = boto3.Session(region_name='us-east-1')
+    translateClient = boto_session.client('translate')
+    print('calling translate')
+    try:
+        response = translateClient.translate_text(
+            Text=txt,
+            SourceLanguageCode='en',
+            TargetLanguageCode=lang
+        )
+        if not "TranslatedText" in response:
+            return None, False
+        else:
+            print(response["TranslatedText"])
+            return response["TranslatedText"], True
+    except ClientError as e:
+        print(e)
+        return None, False
+
+def GetTranslationFromDB(txt, lang):
+    #if translation exists, return translation
+    
+    boto_session = boto3.Session(region_name='us-east-1')
+    dynamodb = boto_session.resource('dynamodb')
+    table = dynamodb.Table('Translations')
+
+    try:
+        response = table.get_item(Key = {"SrcString": txt} )
+        if "ResponseMetadata" in response and "HTTPStatusCode" in response["ResponseMetadata"] and response["ResponseMetadata"]["HTTPStatusCode"] == 200 \
+            and "Item" in response:
+            #valid response and key is found
+            print(response)
+            #here lang is the "column" in the dynamodb table. For example, if "ar" is passed in the "ar" will contain the arabic text. See dynamodb table schema for more info
+            if lang in response["Item"]:
+                #translation exists
+                return response["Item"][lang], True
+            else:
+                #Key exists in table but translation is not found. So fetch the new translation and append it to the existing Key.
+                try:
+                    #Fetch translation
+                    translatedText, isSuccess = GetTranslation(txt, lang)
+                    if isSuccess:
+                        #update Existing Key
+                        try:
+                            updateResponse = table.update_item(
+                                Key={
+                                    "SrcString": txt
+                                },
+                                UpdateExpression='SET ' + lang + ' = :languageCol',
+                                ExpressionAttributeValues={
+                                    ':languageCol': translatedText
+                                }
+                            )
+                            print(updateResponse)
+                            if "ResponseMetadata" in response and "HTTPStatusCode" in response["ResponseMetadata"] and response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+                                return translatedText, True
+                        except Exception as e:
+                            print(e)
+                            
+                except:
+                    return None, False
+        else:
+            #Key does not exist. In this case, we fetch the translation and add a new Key to the table.
+            try:
+                print("Adding new Key")
+                translatedText, isSuccess = GetTranslation(txt, lang)
+                if isSuccess:
+                    #Add new Key and value pair to table
+                    try:
+                        addResponse = table.put_item(
+                            Item={
+                                "SrcString": txt,
+                                lang: translatedText
+                            }
+                        )
+                        print(addResponse)
+                        if "ResponseMetadata" in response and "HTTPStatusCode" in response["ResponseMetadata"] and response["ResponseMetadata"]["HTTPStatusCode"] == 200:
+                            return translatedText, True
+                    except Exception as e:
+                        print(e)
+            except Exception as e:
+                print(e)
+                return None, False
+    except ClientError as e:
+        print(e.response['Error']['Message'])
+        return None, False
+    return None, False
+
+
+@app.route('/translate', methods=['POST'])
+@cross_origin()
+#@Validate_API_Key
+def translate():
+    if request.args is not None:
+        data = request.get_json()
+        if "language" in data and "txt" in data and data["language"] in validLangs:
+            ######
+            print("valid request")
+            txt, isSuccess = GetTranslationFromDB(data["txt"], data["language"])
+            #if Text in DynamoDB
+            #   get translation and return
+            #else
+            #   call Translate
+            #   store in db
+            #   return translation
+            ######
+            if isSuccess:
+                return jsonify(isSuccess= isSuccess, data = txt)
+            else:
+                return jsonify(isSuccess= isSuccess, data = None)
+        else:
+            return "Invalid Request", 400
+    return "Invalid Request", 400
+
+
+
+if __name__ == '__main__':
+    try:
+        #set debug/prod mode
+        if os.getenv('FLASK_ENV') == 'development':
+            application.debug = True
+        else:
+            application.debug = False
+
+        application.run(threaded=True)
+    except Exception as e:
+        logging.error('Check Environment variables. Unable to start.', exc_info=True)
